51b99f2747daa650d81d9a316bfb7aa34512349c
Kaique Covo <kaique.kng@gmail.com>
2022-10-12 12:09:17 -0300

    build(package.json, pages, styles): add css-in-js @stitches and configure

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Main, NextScript, Head } from "next/document";
+import { getCssText } from "../styles";
 
 export default function Document() {
   return (
@@ -14,6 +15,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
         />
+
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        ></style>
       </Head>
       <body>
         <Main />
